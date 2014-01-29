b0f67ee945a8f6d2f88afb6653df87c2cab89341
Blue Prawn <monnier.florent@gmail.com>
2014-01-29 03:08:36 +0100

    test the "Sdlimage.save_png" function relay

---
+++ sdlimage_save_png.mk
@@ -0,0 +1,3 @@
+.PHONY: all
+all:
+	$(MAKE) -f sdlimage_sample.mk FILE=sdlimage_save_png.ml
---
+++ sdlimage_save_png.ml
@@ -0,0 +1,29 @@
+open Sdl
+module Img = Sdlimage
+module Surf = Surface
+
+let () =
+  let width = 320
+  and height = 240 in
+  Sdl.init [`VIDEO];
+  Img.init [`PNG];
+  let surf = Surf.create_rgb ~width ~height ~depth:8 in
+  Random.self_init ();
+  let img = Surf.load_bmp "imgs/caml_icon.bmp" in
+  let w, h = Surf.get_dims img in
+  let rect = Rect.make4 0 0 w h in
+  let rec loop rect i =
+    if i > 0 then begin
+      let x = Random.int (width - w)
+      and y = Random.int (height - h) in
+      let rect = Rect.move rect ~x ~y in
+      Surf.blit_surfs surf img rect;
+      loop rect (i-1)
+    end
+  in
+  loop rect 10;
+  Img.save_png surf ~filename:"saved-surf.png";
+  Img.quit ();
+  Sdl.quit ();
+;;
+
